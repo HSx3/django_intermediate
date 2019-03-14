4c9eee8890c2ebb4f1f743d47388270b8f5d2a88
HSx3 <hs.ssafy@gmail.com>
2019-03-14 06:12:39 +0000

    01_1:N model relation

--- boards/urls.py
+++ boards/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'boards'
 
 urlpatterns = [
-    # path('<int:pk>/update/', views.update, name='update'),
-    path('<int:pk>/edit/', views.edit, name='edit'),    # GET(EDIT) / POST(UPDATE)
-    path('<int:pk>/delete/', views.delete, name='delete'),  # GET 방식 필요 x / POST(DELETE)
-    path('<int:pk>/', views.detail, name='detail'),
-    # path('create/', views.create, name='create'),
-    path('new/', views.new, name='new'),    # GET(NEW) / POST(CREATE)
+    path('<int:board_pk>/comments/<int:comment_pk>/delete/', views.comments_delete, name='comments_delete'),
+    path('<int:board_pk>/comments/', views.comments_create, name='comments_create'),
+    path('<int:board_pk>/edit/', views.edit, name='edit'), # get(edit) / post(update)
+    path('<int:board_pk>/delete/', views.delete, name='delete'), # post(delete)
+    path('<int:board_pk>/', views.detail, name='detail'),
+    path('new/', views.new, name='new'), # GET(new) / POST(create)
     path('', views.index, name='index'),
 ]
